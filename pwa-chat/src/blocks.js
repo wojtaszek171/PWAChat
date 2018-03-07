ba4c640e0e7158628477398e1158e1842b8678c4
wojtaszek171 <pawelwojtaszko@o2.pl>
2018-03-07 09:04:47 +0100

    Manipulating with detectMobile. Created dragging element with jquery touch punch but stopping working after 1 second.

--- pwa-chat/src/blocks.js
+++ pwa-chat/src/blocks.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
+import {findDOMNode} from 'react-dom';
    import axios from 'axios';
    import Pusher from 'pusher-js';
    import ChatList from './ChatDisplayList';
    import ChatBox from './ChatInput';
    import logo from './logo.svg';
    import sendIcon from './ic_send_white_24px.svg';
+   import 'jquery';
+   import 'jquery-ui-dist/jquery-ui';
+   import 'jquery-ui-touch-punch';
    import './App.css';
    import 'onsenui/css/onsenui.css';
    import 'onsenui/css/onsen-css-components.css';
+   import $ from'jquery';
+   import 'jquery-ui';
+   require('jquery-ui-touch-punch');
    var ons = require('onsenui');
    var Ons = require('react-onsenui');
-class Blocks extends Component {
+class Blocks extends React.Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
-
-      }
-
+    const node = findDOMNode(this.refs.toggle);
+    $(node).draggable();
+    //$( "#draggable" ).draggable();
+  }
 
       render() {
 
@@ -25,18 +33,30 @@ class Blocks extends Component {
 
         if(mobile===true){
           return (
-            <div className="App" style={{background:'blue'}}>
+            <div id="draggable" ref="toggle" style={{width:'50px',height:'50px', background:'#000000', cursor:'pointer'}}></div>
 
-            </div>
+
           );
         }else{
             return (
-              <div className="App" style={{background:'blue'}}>
+
+              <div className="App">
+
+              <div data-role="content">
+
+              <div id="draggable" ref="toggle" style={{width:'50px',height:'50px', background:'#000000', left:'0', top:'0', 'color':'white'}}>gdsfds</div>
+
+
 
               </div>
+              </div>
+
             );
+
           }
         }
+
+
 }
 
 export default Blocks;
